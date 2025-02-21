c7222ebd5a0d8bbb0c3077a93565e83aae4f6c74
DevNuwancat <shashika90nuwan@gmail.com>
2025-02-21 18:48:48 +0530

    Initial commit: Add Nuwan Colors Package

---
+++ test.py
@@ -0,0 +1,34 @@
+from nuwan_colors import Colors
+from nuwan_colors import Texts
+
+print(Colors.black("Hello World"))
+print(Colors.red("Hello World"))
+print(Colors.green("Hello World"))
+print(Colors.yellow("Hello World"))
+print(Colors.blue("Hello World"))
+print(Colors.magneta("Hello World"))
+print(Colors.cyan("Hello World"))
+print(Colors.white("Hello World"))
+print(Colors.bg_black("Hello World"))
+print(Colors.bg_red("Hello World"))
+print(Colors.bg_green("Hello World"))
+print(Colors.bg_yellow("Hello World"))
+print(Colors.bg_blue("Hello World"))
+print(Colors.bg_magneta("Hello World"))
+print(Colors.bg_cyan("Hello World"))
+print(Colors.bg_white("Hello World"))
+print(Colors.bg_reverse_white("Hello World"))
+
+print(Texts.bold("Hello World"))
+print(Texts.underline("Hello World"))
+print(Texts.italic("Hello World"))
+print(Texts.concealed("Hello World"))
+print(Texts.blink("Hello World"))
+
+
+print(
+    Colors.green(
+        Texts.italic("Hello World")
+    )
+)
+
